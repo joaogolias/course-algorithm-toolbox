121cd62c7449e9a2b641a5a2ff318dbf1b2af5d8
Golias <joao.victor@mobile2you.com.br>
2020-08-25 21:00:12 -0300

    added gitignore

--- src/week1/sum-of-two-digits/main.cpp
+++ src/week1/sum-of-two-digits/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-using namespace std;
+
+int sum_of_two_digits(int first_digit, int second_digit)
+{
+	return first_digit + second_digit;
+}
 
 int main()
 {
-	cout << "Hello World" << endl;
+	int a = 0;
+	int b = 0;
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << sum_of_two_digits(a, b);
 	return 0;
 }
